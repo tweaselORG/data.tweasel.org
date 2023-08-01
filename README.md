b34e5b37ce9bfbe998a998a638a137f244d3a8c7
Benjamin Altpeter <hi@bn.al>
2023-08-01 14:50:10 +0200

    Initial commit: Basic setup

---
+++ README.md
@@ -0,0 +1,38 @@
+# Tweasel open data Datasette instance
+
+This repository contains the scripts for hosting, and configuration of the Tweasel open data Datasette instance hosted at [data.tweasel.org](https://data.tweasel.org). The homepage contains a more detailed description of what the instance is about. For technical details about how it was set up, see [tweaselORG/meta#33](https://github.com/tweaselORG/meta/issues/33).
+
+## Creating the main database
+
+The main database is created dynamically, combining the datasets into a single `requests` table. To create the database, you need to have all the individual SQLite databases of the datasets (specified in `datasets.json`) in the `datasets` folder (they are not included in the repo but archived in a B2 bucket).
+
+Then, you can run `yarn make-database` to create the database. This will (re-)create and correctly configure `datasette/data.db`.
+
+## Running Datasette
+
+We are using Datasette in [configuration directory mode](https://docs.datasette.io/en/stable/settings.html#configuration-directory-mode), as such you only need to pass that folder to get the correct configuration:
+
+```sh
+datasette datasette/
+```
+
+## Adding a new dataset
+
+To add a new dataset, you need to:
+
+1. Create an SQLite database of the requests in the dataset in the correct format. See: [example of creating such a database from HAR files](https://github.com/tweaselORG/experiments/blob/main/monkey-july-2023/export-to-db.ts), [examples of creating such a database from an existing PostgreSQL database](https://github.com/tweaselORG/meta/issues/33)
+2. Upload the SQLite database to the B2 bucket.
+3. Store the SQLite database in the `datasets` folder (with a name of `<slug>.db`).
+4. Add the dataset and its metadata to `datasets.json`.
+5. Mention the dataset in `datasette/templates/index.html` (in the "Datasets" section).
+6. Run `yarn make-database` to recreate the main database.
+
+## Plugins
+
+We are using the following Datasette plugins:
+
+* datasette-copyable
+* datasette-graphql
+* datasette-jq
+* datasette-sqlite-regex
+* datasette-cors
---
+++ scripts/make-database.ts
@@ -0,0 +1,96 @@
+import Database from 'better-sqlite3';
+import yesno from 'yesno';
+import * as sqlite_regex from 'sqlite-regex';
+import fse from 'fs-extra';
+import datasets from '../datasets.json';
+
+(async () => {
+    if (await fse.pathExists('datasette/data.db')) {
+        const ok = await yesno({
+            question: 'This will delete and recreate your existing `datasette/data.db`. Do you want to continue?',
+        });
+        if (!ok) process.exit(1);
+        await fse.remove('datasette/data.db');
+    }
+
+    const db = new Database('datasette/data.db');
+    db.loadExtension(sqlite_regex.getLoadablePath());
+    db.pragma('journal_mode = WAL');
+
+    // Create and fill `datasets` table.
+    db.exec(`
+create table "datasets" (
+    "slug" text,
+    "title" text not null,
+    "description" text not null,
+    "url" text not null,
+    "sourceCodeUrl" text,
+    primary key("slug")
+) without rowid;
+`);
+
+    const insertDataset = db.prepare(`
+insert into "datasets" ("slug", "title", "description", "url", "sourceCodeUrl")
+values (?, ?, ?, ?, ?);
+`);
+    for (const d of datasets) insertDataset.run(d.slug, d.title, d.description, d.url, d.sourceCodeUrl);
+
+    // Create and fill `requests` table.
+    db.exec(`
+create table "requests" (
+    "id" text,
+    "dataset" text,
+    "initiator" text,
+    "platform" text not null,
+    "runType" text,
+    "startTime" text not null,
+    "method" text not null,
+    "httpVersion" text,
+    "endpointUrl" text,
+    "scheme" text,
+    "host" text not null,
+    "port" integer,
+    "path" text not null,
+    "content" blob,
+    "headers" text not null,
+    "cookies" text not null,
+    foreign key("dataset") references "datasets"("slug"),
+    primary key("dataset", "id")
+) without rowid;
+`);
+
+    for (const d of datasets) db.exec(`attach './datasets/${d.slug}.db' as '${d.slug}';`);
+
+    db.exec(`
+insert into requests
+    select id, dataset, initiator, platform, runType, startTime, method, httpVersion, endpointUrl, scheme, host, port, path, content, headers, cookies from (
+        with vendors as materialized (
+            select id, dataset, initiator, platform, runType, startTime, method, httpVersion, endpointUrl, scheme, host, port, path, content, headers, cookies,
+            case
+                when initiator is null then null
+                when instr(initiator,'.') = 0 then initiator
+                else regex_replace('\\.[^.]+@.+?$', initiator, '')
+              end as vendor,
+             -- For the 'do-they-track' requests, we don't know the scheme. But it really doesn't make sense to consider that for this, anyway.
+             regex_replace('\\?.+$', host || path, '')  as _endpointUrl
+            from (
+                ${datasets.map((d) => `select * from '${d.slug}'.requests`).join('\nunion all\n')}
+            )
+        )
+
+        select * from vendors where
+            -- Only include requests that are made to the same endpointUrl by apps from at least two different vendors (https://github.com/tweaselORG/meta/issues/33#issuecomment-1658348929).
+            _endpointUrl in (
+                select _endpointUrl from vendors group by _endpointUrl having count(distinct vendor) >= 2
+                union
+                select _endpointUrl from vendors where vendor is null
+            )
+            -- Filter out iOS system background traffic as that may contain authentication values (https://github.com/tweaselORG/meta/issues/33#issuecomment-1660099572),
+            and (not platform = 'ios' or initiator is not null)
+    );
+`);
+
+    // Create indexes to speed up facets (https://docs.datasette.io/en/stable/facets.html#speeding-up-facets-with-indexes).
+    for (const facetColumn of ['method', 'dataset', 'runType', 'scheme', 'platform'])
+        db.exec(`create index "requests_${facetColumn}" on "requests"("${facetColumn}");`);
+})();
